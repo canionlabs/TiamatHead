47053bba92e37125d3c73a6542406219e5cc8f87
caiovictormc <caiovictormc@gmail.com>
2018-10-22 19:29:18 -0300

    Add web to allowed_hosts

--- monar_back/settings.py
+++ monar_back/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['web']
 
 
 # Application definition
