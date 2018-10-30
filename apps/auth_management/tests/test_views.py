9693297028013a0b2e7d7b0c968f129619e41f38
caiovictormc <caiovictormc@gmail.com>
2018-10-30 17:19:35 -0300

    auth_management: Add tests to GroupListView

--- apps/auth_management/tests/test_views.py
+++ apps/auth_management/tests/test_views.py
@@ -1,4 +1,6 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+
 from django.urls import reverse
 from django.utils import timezone
 
@@ -19,6 +21,10 @@ UserModel = get_user_model()
 
 class BaseTest(APITestCase):
     def setUp(self):
+        self.test_group = Group.objects.create(
+            name='Test Group'
+        )
+
         self.test_superuser = UserModel.objects.create_superuser(
             username='admintest',
             email='admintest@test.ts', password='admsuper-@pass2s'
@@ -59,7 +65,7 @@ class BaseTest(APITestCase):
             "HTTP_AUTHORIZATION": f"Bearer {self.valid_user_token.token}",
         }
 
-        self.adm_urls = ['list-users', 'detail-users']
+        self.adm_urls = ['list-users', 'detail-users', 'list-groups']
     
     def tearDown(self):
         self.test_superuser.delete()
@@ -83,6 +89,16 @@ class TestUserManagementView(BaseTest):
                     **self.auth_user_headers
                 )
             self.assertEqual(response.status_code, 403)
+
+    def test_group_list(self):
+        response = self.client.get(
+            reverse('auth_management:list-groups'),
+            **self.auth_admin_headers
+        )
+
+        self.assertEqual(response.status_code, 200)
+        content = response.json()
+        self.assertEqual(len(content), Group.objects.all().count())
 
     def test_user_list(self):
         response = self.client.get(
