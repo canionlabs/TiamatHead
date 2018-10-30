8aeddc7fcc1ea225f5c67bba2d44c962572725f1
caiovictormc <caiovictormc@gmail.com>
2018-10-30 17:19:10 -0300

    auth_management.urls: Update views name

--- apps/auth_management/urls.py
+++ apps/auth_management/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 
-from apps.auth_management.views import UserList, UserDetails, GroupList
+from apps.auth_management.views import UserListView, \
+    UserDetailsView, GroupListView
+
 
 app_name='auth_management'
 
 urlpatterns = [
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
-    path('users/', UserList.as_view(), name='list-users'),
-    path('users/<pk>/', UserDetails.as_view(), name='detail-users'),
-    path('groups/', GroupList.as_view(), name='list-groups'),
+    path('users/', UserListView.as_view(), name='list-users'),
+    path('users/<pk>/', UserDetailsView.as_view(), name='detail-users'),
+    path('groups/', GroupListView.as_view(), name='list-groups'),
 ]
