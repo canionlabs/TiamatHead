f8cf163e3cc721535a9c8c7e0509b8ea238ac8a8
caiovictormc <caiovictormc@gmail.com>
2018-11-01 18:47:38 -0300

    Creating CRU views

--- apps/devices/views.py
+++ apps/devices/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions, serializers
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, TokenHasScope
 
-# Create your views here.
+from apps.devices.permissions import IsOwnerOrReadOnly
+from apps.devices.models import Device
+from apps.devices.serializers import DeviceSerializer
+
+
+class DeviceListView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    serializer_class = DeviceSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Device.objects.filter(user=user)
+    
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class DeviceDetailView(generics.RetrieveUpdateAPIView):
+    permission_classes = [
+        permissions.IsAuthenticated, TokenHasReadWriteScope, IsOwnerOrReadOnly
+    ]
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
