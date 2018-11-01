c6196c7a41d586d51d0ba46c3ecb86d84a0ab9f2
caiovictormc <caiovictormc@gmail.com>
2018-11-01 18:45:44 -0300

    Device: device_id as uuid field

--- apps/devices/models.py
+++ apps/devices/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+import uuid
+
 
 class Device(models.Model):
+    device_id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, 
+        editable=False, verbose_name='Device ID'
+    )
     name = models.CharField(max_length=75, null=True)
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='devices'
     )
-    device_id = models.CharField(max_length=275, verbose_name='Device ID')
-    token = models.CharField(max_length=275)
 
-    def __str__(self): 
-        return f'{self.device_id or self.name}'
+    def __str__(self):
+        return f'{self.device_id}'
