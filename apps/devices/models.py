8255b7f47242875121a2273b7303a9b5ea1028fc
caiovictormc <caiovictormc@gmail.com>
2018-10-31 21:41:15 -0300

    Creating basic device model

--- apps/devices/models.py
+++ apps/devices/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Device(models.Model):
+    name = models.CharField(max_length=75, null=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='devices'
+    )
+    device_id = models.CharField(max_length=275, verbose_name='Device ID')
+    token = models.CharField(max_length=275)
+
+    def __str__(self): 
+        return f'{self.device_id or self.name}'
