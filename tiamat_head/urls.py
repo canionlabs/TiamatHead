97ea1db3d376b09cce7c06b6e130fd8f6fb2bcd8
caiovictormc <caiovictormc@gmail.com>
2018-11-06 14:16:03 -0300

    urls docs

--- tiamat_head/urls.py
+++ tiamat_head/urls.py
@@ -36,7 +36,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('docs/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', include('apps.auth_management.urls', namespace='auth_management')),
     path('', include('apps.devices.urls', namespace='devices'))
 ]
