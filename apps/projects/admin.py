685c6ee0d5f9d53b79cb80833a290fb877529324
caiovictormc <caiovictormc@gmail.com>
2018-11-07 18:43:09 -0300

    projects: Add Project to admin

--- apps/projects/admin.py
+++ apps/projects/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.projects.models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id')
+    fields = ('id', 'name', 'script')
