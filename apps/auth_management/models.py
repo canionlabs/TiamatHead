f2d213dc42bd5953c8d4763f2ebbc6b13b1acad5
caiovictormc <caiovictormc@gmail.com>
2018-11-07 18:18:42 -0300

    auth_management: Creating model Organization

--- apps/auth_management/models.py
+++ apps/auth_management/models.py
@@ -1,1 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+from apps.common.models import DefaultModel
+
+
+class Organization(DefaultModel):
+    name = models.CharField(max_length=175)
+    users = models.ManyToManyField(User, related_name='organizations')
+
+    def __str__(self):
+        return '{self.name}'
