32157d68a9b75507f5bf5689c10f87608df3b411
caiovictormc <caiovictormc@gmail.com>
2018-11-07 18:19:02 -0300

    projects: Creating model Project

--- apps/projects/models.py
+++ apps/projects/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.postgres.fields import JSONField
 
-# Create your models here.
+from apps.common.models import DefaultModel
+from apps.auth_management.models import Organization
+
+import uuid
+
+
+class Project(DefaultModel):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False,
+        verbose_name='Project ID'
+    )
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='projects'
+    )
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name='projects'
+    )
+    name = models.CharField(max_length=175)
+    script = JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return '{self.id}'
