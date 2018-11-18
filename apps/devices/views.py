b01c4b56a31a2917ec76bf855b4d4a9c102fdfd6
caiovictormc <caiovictormc@gmail.com>
2018-11-18 10:23:51 -0300

    Filtering devices in DeviceListView

--- apps/devices/views.py
+++ apps/devices/views.py
@@ -1,31 +1,59 @@
 from rest_framework import generics, permissions
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
 
-from apps.devices.permissions import IsOwnerOrReadOnly
+from apps.devices.permissions import IsOwnerOrReadOnly, IsOrganizationMember
 from apps.devices.models import Device
 from apps.devices.serializers import DeviceSerializer
 
 
 class DeviceListView(generics.ListCreateAPIView):
     """
-    List devices related with users projects
+    list:
+    List related devices
+
+    create:
+    Create devices for new projects or existing projects related with the user
     """
     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
     serializer_class = DeviceSerializer
 
+    def check_params(self):
+        formated_params = {}
+
+        # valid_params = {'API Param Name': 'Model Field Reference'}
+        valid_params = {
+            'project_id': 'project__id'
+        }
+        query_params = self.request.query_params
+
+        for valid_key, field_ref in valid_params.items():
+            param = query_params.get(valid_key, None)
+            if param:
+                formated_params.update({field_ref: param})
+        return formated_params
+
     def get_queryset(self):
         user = self.request.user
+        params = self.check_params()
+
         if user.is_superuser:
-            return Device.objects.all()
-        return Device.objects.filter(project__organization__users=user)
+            return Device.objects.filter(**params)
+
+        return Device.objects.filter(
+            project__organization__users=user, **params
+        )
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
 
 class DeviceDetailView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    Retrieve devices related with users organization
+    """
     permission_classes = [
-        permissions.IsAuthenticated, TokenHasReadWriteScope, IsOwnerOrReadOnly
+        permissions.IsAuthenticated, 
+        TokenHasReadWriteScope, IsOrganizationMember
     ]
     queryset = Device.objects.all()
     serializer_class = DeviceSerializer
