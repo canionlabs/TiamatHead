4a755182e3660ff87151125442b51485ed67d215
caiovictormc <caiovictormc@gmail.com>
2018-11-18 10:23:00 -0300

    Creating permission IsOrganizationMember

--- apps/devices/permissions.py
+++ apps/devices/permissions.py
@@ -12,3 +12,15 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
 
         return obj.user == request.user
+
+
+class IsOrganizationMember(permissions.BasePermission):
+    """
+    Object-level permission to only allow organization members of an object to edit it.
+    Assumes the model instance has an `project` attribute.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_superuser:
+            return True
+        return obj.project.organization in request.user.organizations.all()
