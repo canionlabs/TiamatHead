5db292ac5c9767dce8728d4d21d9710c91557cc4
caiovictormc <caiovictormc@gmail.com>
2018-11-18 10:19:41 -0300

    devices: Adjustments in DeviceSerializer

--- apps/devices/serializers.py
+++ apps/devices/serializers.py
@@ -1,22 +1,16 @@
+from django.contrib.auth.models import User
+
 from rest_framework import serializers
 
 from apps.devices.models import Device
-from django.contrib.auth.models import User
-
-
-class UserMinimalSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ("username", "id")
+from apps.projects.serializers import ProjectMinimalSerializer
+from apps.auth_management.serializers import UserMinimalSerializer
 
 
 class DeviceSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-    device_id = serializers.UUIDField(format="hex_verbose", read_only=True)
+    id = serializers.UUIDField(format="hex_verbose", read_only=True)
+    project = ProjectMinimalSerializer()
 
     class Meta:
         model = Device
-        fields = ("name", "user", "device_id")
-
-    def get_user(self, obj):
-        return UserMinimalSerializer(obj.user).data
+        fields = ("id", "name", "project")
