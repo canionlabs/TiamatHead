af3da3b6c4e6c591aa8b770828efe4228bfa97d2
caiovictormc <caiovictormc@gmail.com>
2018-11-19 17:28:00 -0300

    Creating serializer DeviceCreateSerializer

--- apps/devices/serializers.py
+++ apps/devices/serializers.py
@@ -3,14 +3,29 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 from apps.devices.models import Device
+from apps.projects.models import Project
 from apps.projects.serializers import ProjectMinimalSerializer
 from apps.auth_management.serializers import UserMinimalSerializer
 
 
-class DeviceSerializer(serializers.ModelSerializer):
+class DeviceListSerializer(serializers.ModelSerializer):
     device_id = serializers.UUIDField(source="id", format="hex_verbose", read_only=True)
     project = ProjectMinimalSerializer()
 
     class Meta:
         model = Device
         fields = ("device_id", "name", "project")
+
+
+class DeviceCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Device
+        fields = ("device_id", "name", "project")
+
+    def create(self, validated_data):
+        project = validated_data.pop('project')
+        instance = Device.objects.create(project=project, **validated_data)
+        if project:
+            instance.project = project
+
+        return instance
