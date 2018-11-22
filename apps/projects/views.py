8563aae580a425c48cceac8bef7541cf43f41739
caiovictormc <caiovictormc@gmail.com>
2018-11-22 16:30:21 -0300

    projects: Creating ProjectListCreateView

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
 
-# Create your views here.
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
+
+from apps.projects.serializers import ProjectSerializer
+from apps.projects.models import Project
+
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    """
+    list:
+    List related projects.
+
+    create:
+    Create a project for a related organization.
+    """
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    serializer_class = ProjectSerializer
+    queryset = Project.objects.all()
