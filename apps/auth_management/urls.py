760d1634e04cefecbd62371cafeddcf952573bb2
caiovictormc <caiovictormc@gmail.com>
2018-11-26 16:11:56 -0300

    auth_management: Creating OrganizationListCreateView

--- apps/auth_management/urls.py
+++ apps/auth_management/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 
 from apps.auth_management.views import UserListView, \
-    UserDetailsView, GroupListView
+    UserDetailsView, GroupListView, OrganizationListCreateView
 
 
 app_name='auth_management'
@@ -11,4 +11,8 @@ urlpatterns = [
     path('users/', UserListView.as_view(), name='list-users'),
     path('users/<pk>/', UserDetailsView.as_view(), name='detail-users'),
     path('groups/', GroupListView.as_view(), name='list-groups'),
+
+    path('organizations/',
+        OrganizationListCreateView.as_view(), name='list-create-organizations'
+    )
 ]
--- apps/auth_management/views.py
+++ apps/auth_management/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User, Group
 
 from rest_framework import generics, permissions
+from django_filters import rest_framework as filters
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
 
-from apps.auth_management.serializers import UserSerializer, GroupSerializer
+from apps.auth_management.models import Organization
+from apps.auth_management.serializers import UserSerializer, \
+    GroupSerializer, OrganizationSerializer
 
 
 class UserListView(generics.ListCreateAPIView):
@@ -22,3 +25,15 @@ class GroupListView(generics.ListAPIView):
     permission_classes = [permissions.IsAdminUser, TokenHasReadWriteScope]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+
+
+class OrganizationListCreateView(generics.ListCreateAPIView):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    serializer_class = OrganizationSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser:
+            return Organization.objects.all()
+        return user.organizations.all()
