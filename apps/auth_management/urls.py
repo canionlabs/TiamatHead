bc3e09f17483fdba7d54b180d51c746c575706a3
caiovictormc <caiovictormc@gmail.com>
2019-01-21 19:57:09 -0300

    auth_m: Creating OrganizationViewSet

--- apps/auth_management/urls.py
+++ apps/auth_management/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
+
 from apps.auth_management.views import UserListView, \
-    UserDetailsView, GroupListView, OrganizationListCreateView , \
-    OrganizationAddUsersView, OrganizationRemoveUsersView
+    UserDetailsView, GroupListView, OrganizationAddUsersView, \
+    OrganizationRemoveUsersView, OrganizationViewSet
 
 
-
-app_name='auth_management'
+app_name = 'auth_management'
+router = DefaultRouter()
+router.register('organizations', OrganizationViewSet, 'organization')
 
 urlpatterns = [
     path('o/', include('oauth2_provider.urls', namespace='oauth2_provider')),
@@ -14,18 +17,20 @@ urlpatterns = [
     path('users/<pk>/', UserDetailsView.as_view(), name='detail-users'),
     path('groups/', GroupListView.as_view(), name='list-groups'),
 
-    path(
-        'organizations/',
-        OrganizationListCreateView.as_view(), name='list-create-organizations'
-    ),
-    path(
-        'organizations/<pk>/users/',
-        OrganizationAddUsersView.as_view(),
-        name='create-organization-users'
-    ),
-    path(
-        'organizations/<pk>/users/<user-pk>/',
-        OrganizationRemoveUsersView.as_view(),
-        name='remove-organization-users'
-    ),
+    # path(
+    #     'organizations/',
+    #     OrganizationListCreateView.as_view(), name='list-create-organizations'
+    # ),
+    # path(
+    #     'organizations/<pk>/users/',
+    #     OrganizationAddUsersView.as_view(),
+    #     name='create-organization-users'
+    # ),
+    # path(
+    #     'organizations/<pk>/users/<user-pk>/',
+    #     OrganizationRemoveUsersView.as_view(),
+    #     name='remove-organization-users'
+    # ),
 ]
+
+urlpatterns += router.urls
--- apps/auth_management/views.py
+++ apps/auth_management/views.py
@@ -1,12 +1,13 @@
 from django.contrib.auth.models import User, Group
 
 from rest_framework.response import Response
-from rest_framework import generics, permissions, status
+from rest_framework import generics, permissions, status, viewsets
 from django_filters import rest_framework as filters
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
 
 from apps.auth_management.models import Organization
-from apps.auth_management.serializers import UserSerializer, \
+from apps.auth_management.filters import OrganizationFilter
+from apps.auth_management.serializers import UserSerializer, UserMinimalSerializer, \
     GroupSerializer, OrganizationSerializer, OrganizationUserSerializer
 
 
@@ -57,13 +58,36 @@ class OrganizationRemoveUsersView(generics.DestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class OrganizationListCreateView(generics.ListCreateAPIView):
-    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+# class OrganizationListCreateView(generics.ListCreateAPIView):
+#     permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+#     serializer_class = OrganizationSerializer
+#     filter_backends = (filters.DjangoFilterBackend,)
+
+#     def get_queryset(self):
+#         user = self.request.user
+#         if user.is_superuser:
+#             return Organization.objects.all()
+#         return user.organizations.all()
+
+
+class OrganizationViewSet(viewsets.ModelViewSet):
+    permissions_classes = [
+        permissions.IsAuthenticated, TokenHasReadWriteScope
+    ]
     serializer_class = OrganizationSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = [filters.DjangoFilterBackend, ]
+    filterset_class = OrganizationFilter
 
     def get_queryset(self):
         user = self.request.user
         if user.is_superuser:
             return Organization.objects.all()
         return user.organizations.all()
+
+    def perform_create(self, serializer):
+        users = [self.request.user.username, ]
+
+        user_list = self.request.data.getlist('users')
+        if user_list:
+            users += user_list
+        serializer.save(users=users)
